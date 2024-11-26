d8fe9fcc34bf5bfd727ab979f953641ba2a8f102
mariazakria <marizakria130@gmail.com>
2024-11-26 05:33:50 +0200

    Fix base path for Vercel deployment

--- src/Pages/Notfound/Notfound.jsx
+++ src/Pages/Notfound/Notfound.jsx
@@ -4,7 +4,7 @@ export default function Notfound() {
   return (
     <div className='mt-20 py-20 flex justify-center items-center flex-col'>
       <img className='w-96' src={notfound} alt='not found page'/> 
-      <h2 className='text-center mt-4 text-2xl text-gray-600'>Oops! The page you're looking for doesn't exist. <a className='text-blue-600' href='/home'>Back To Home</a></h2>
+      <h2 className='text-center mt-4 text-2xl text-gray-600'>Oops! The page you're looking for doesn't exist. <a className='text-blue-600' href='/'>Back To Home</a></h2>
       
 
     </div>
