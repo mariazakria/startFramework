532c4e58eed69b9a64288d10d3e006de9ed7c2d2
mariazakria <marizakria130@gmail.com>
2024-11-27 04:23:54 +0200

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,61 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Notfound from './Pages/Notfound/Notfound';
 import Layout from './Layouts/layouts'; 
 import Portfolio from './Pages/Portfolio/Portfolio';
 import Home from './Pages/Home/Home';
 import Contact from './Pages/Home/Contact/Contact';
 import About from './Pages/About/About';
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+        {
+          path: "portfolio",
+          element: <Portfolio />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "*",
+          element: <Notfound />,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-        <Route path="/About" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-
-          <Route index element={<Home />} /> 
-          <Route path="*" element={<Notfound />} />
-
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
 export default App;
+
+// Old method
+
+ // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Layout />}>
+    //     <Route path="/About" element={<About />} />
+    //     <Route path="/portfolio" element={<Portfolio />} />
+    //     <Route path="/contact" element={<Contact />} />
+
+    //       <Route index element={<Home />} /> 
+    //       <Route path="*" element={<Notfound />} />
+
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
